85574f151b9bbb894b165cd4884b6872597a3fe5
efraimsombra <efraim.sombra456@gmail.com>
2026-02-24 13:50:17 -0300

    Refactor data loading and dashboard setup

--- import streamlit as st.py
+++ import streamlit as st.py
@@ -5,15 +5,6 @@ from datetime import datetime
 import subprocess
 import sys
 
-# --- TRUQUE: Forçar a instalação do openpyxl ---
-try:
-    import openpyxl
-except ImportError:
-    st.warning("Instalando o openpyxl... Aguarde um instante.")
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "openpyxl"])
-    import openpyxl
-    st.success("Instalação concluída! Recarregando...")
-    st.rerun()
 
 # --- Configuração da Página ---
 st.set_page_config(
@@ -307,4 +298,5 @@ if not df_dre_filtrado.empty:
     st.dataframe(df_dre_filtrado[['Nome do Cliente', 'Origem do Processo', 'Data', 'Responsável', 'Valor do Proposta origem']])
 else:
     st.warning("Não há dados de vendas para o período selecionado.")
+
 
